7a068864f3542299b31a66d10d6691da2dfad939
Calvin <179209347@qq.com>
2021-08-03 23:50:53 +0800

    Add fire_smoke, reflective_vest, safety_helmet detection sdks.

---
+++ sec_sdks/safety_helmet_sdk_cpu/src/main/java/me/calvin/example/SmallSafetyHelmetDetectExample.java
@@ -0,0 +1,67 @@
+package me.calvin.example;
+
+import ai.djl.ModelException;
+import ai.djl.inference.Predictor;
+import ai.djl.modality.cv.Image;
+import ai.djl.modality.cv.ImageFactory;
+import ai.djl.modality.cv.output.BoundingBox;
+import ai.djl.modality.cv.output.DetectedObjects;
+import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ModelZoo;
+import ai.djl.repository.zoo.ZooModel;
+import ai.djl.translate.TranslateException;
+import me.calvin.aias.SmallSafetyHelmetDetect;
+import me.calvin.aias.util.ImageUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 安全帽检测例子
+ *
+ * 目录：http://aias.top/
+ *
+ * @author Calvin
+ */
+public final class SmallSafetyHelmetDetectExample {
+
+  private static final Logger logger = LoggerFactory.getLogger(SmallSafetyHelmetDetectExample.class);
+
+  private SmallSafetyHelmetDetectExample() {}
+
+  public static void main(String[] args) throws IOException, ModelException, TranslateException {
+    Path imageFile = Paths.get("src/test/resources/safety_helmet.jpg");
+    Image image = ImageFactory.getInstance().fromFile(imageFile);
+
+    Criteria<Image, DetectedObjects> criteria = new SmallSafetyHelmetDetect().criteria(image);
+
+    try (ZooModel model = ModelZoo.loadModel(criteria);
+        Predictor<Image, DetectedObjects> predictor = model.newPredictor()) {
+      DetectedObjects detections = predictor.predict(image);
+      List<DetectedObjects.DetectedObject> items = detections.items();
+
+      List<String> names = new ArrayList<>();
+      List<Double> prob = new ArrayList<>();
+      List<BoundingBox> boxes = new ArrayList<>();
+      for (int i = 0; i < items.size(); i++) {
+        DetectedObjects.DetectedObject item = items.get(i);
+        if (item.getProbability() < 0.3f) {
+          continue;
+        }
+        names.add(item.getClassName() + " " + item.getProbability());
+        prob.add(item.getProbability());
+        boxes.add(item.getBoundingBox());
+      }
+
+      detections = new DetectedObjects(names, prob, boxes);
+      ImageUtils.saveBoundingBoxImage(image, detections, "safety_helmet_result_s.png", "build/output");
+
+      logger.info("{}", detections);
+    }
+  }
+}
