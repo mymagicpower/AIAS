16ffeb0fe9c1e00f2074dad62bd78a0faf262c4f
Calvin <179209347@qq.com>
2021-08-06 18:05:12 +0800

    Add Sentiment Classification sdk.

---
+++ nlp_sdks/senta_bilstm_sdk/README.md
@@ -0,0 +1,33 @@
+# 文本 - 情感分析SDK [中文]
+情感倾向分析（Sentiment Classification，简称Senta）针对带有主观描述的中文文本，
+可自动判断该文本的情感极性类别并给出相应的置信度，能够帮助企业理解用户消费习惯、
+分析热点话题和危机舆情监控，为企业提供有利的决策支持。
+
+![image](https://djl-model.oss-cn-hongkong.aliyuncs.com/AIAS/nlp_sdks/sentiment_analysis.jpeg)
+
+### SDK算法：
+该模型基于一个双向LSTM结构，情感类型分为：
+- 消极（negative）
+- 积极（positive）
+
+## 运行例子 - SentaBilstmExample
+运行成功后，命令行应该看到下面的信息:
+```text
+...
+[INFO ] - 输入句子: 这家餐厅很好吃
+#中文分词
+[INFO ] - Words : [这家, 餐厅, 很好吃]
+
+#词性标注
+[INFO ] - Tags : [r, n, a]
+
+#情感分析概率
+[INFO ] - [0.059312407, 0.9406876]
+
+[INFO ] - negative : 0.059312407
+[INFO ] - positive : 0.9406876
+
+```
+
+### 帮助 
+添加依赖库：lib/aias-senta-bilstm-lib-0.1.0.jar
---
+++ nlp_sdks/senta_bilstm_sdk/src/main/java/me/calvin/example/SentaBilstmExample.java
@@ -0,0 +1,52 @@
+package me.calvin.example;
+
+import ai.djl.ModelException;
+import ai.djl.inference.Predictor;
+import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ZooModel;
+import ai.djl.translate.TranslateException;
+import me.calvin.aias.Lac;
+import me.calvin.aias.SentaBilstm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public final class SentaBilstmExample {
+
+  private static final Logger logger = LoggerFactory.getLogger(SentaBilstmExample.class);
+
+  private SentaBilstmExample() {}
+
+  public static void main(String[] args) throws IOException, TranslateException, ModelException {
+
+    // 分词
+    Lac lac = new Lac();
+    Criteria<String, String[][]> lacCriteria = lac.criteria();
+    // 情感分析
+    SentaBilstm senta = new SentaBilstm();
+    Criteria<String[], float[]> SentaCriteria = senta.criteria();
+
+    try (ZooModel<String, String[][]> lacModel = lacCriteria.loadModel();
+        Predictor<String, String[][]> lacPredictor = lacModel.newPredictor();
+        ZooModel<String[], float[]> sentaModel = SentaCriteria.loadModel();
+        Predictor<String[], float[]> sentaPredictor = sentaModel.newPredictor()) {
+
+      String input = "这家餐厅很好吃";
+      logger.info("输入句子: {}", input);
+
+      String[][] lacResult = lacPredictor.predict(input);
+      // 分词
+      logger.info("Words : " + Arrays.toString(lacResult[0]));
+      logger.info("Tags : " + Arrays.toString(lacResult[1]));
+
+      // 情感分析
+      float[] sentaResult = sentaPredictor.predict(lacResult[0]);
+      logger.info(Arrays.toString(sentaResult));
+      logger.info("negative : " + sentaResult[0]);
+      logger.info("positive : " + sentaResult[1]);
+      
+    }
+  }
+}
