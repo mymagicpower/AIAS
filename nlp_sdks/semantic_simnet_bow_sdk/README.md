3a8683d114f248e092d8cacecb3ea5a9f950465d
Calvin <179209347@qq.com>
2021-08-13 15:47:18 +0800

    Sentence similarity calculation.

---
+++ nlp_sdks/semantic_simnet_bow_sdk/README.md
@@ -0,0 +1,44 @@
+# 文本 - 短文本相似度SDK [中文]
+计算两个句子的cosin相似度:
+可以根据用户输入的两个文本，计算出相似度得分。
+
+![image](https://djl-model.oss-cn-hongkong.aliyuncs.com/AIAS/nlp_sdks/Universal-Sentence-Encoder.png)
+
+### SDK算法：
+该模型基于SimNet，是一个计算句子相似度的模型。
+
+## 运行例子 - SemanticExample
+运行成功后，命令行应该看到下面的信息:
+```text
+...
+[INFO ] - 句子 1: 这个棋局太难了
+#中文分词
+[INFO ] - Words : [这个, 棋局, 太难, 了]
+#词性标注
+[INFO ] - Tags : [r, n, a, xc]
+
+[INFO ] - 句子 2: 这个棋局不简单
+#中文分词
+[INFO ] - Words : [这个, 棋局, 不, 简单]
+#词性标注
+[INFO ] - Tags : [r, n, d, a]
+
+[INFO ] - 句子 3: 这个棋局很有意思
+#中文分词
+[INFO ] - Words : [这个, 棋局, 很, 有意思]
+#词性标注
+[INFO ] - Tags : [r, n, d, a]
+
+#计算短文本相似度
+[INFO ] - 句子 1: 这个棋局太难了
+[INFO ] - 句子 2: 这个棋局不简单
+[INFO ] - 相似度 : 0.8542996
+
+[INFO ] - 句子 1: 这个棋局太难了
+[INFO ] - 句子 3: 这个棋局很有意思
+[INFO ] - 相似度 : 0.8260221
+
+```
+
+### 帮助 
+添加依赖库：lib/aias-semantic-simnet-bow-lib-0.1.0.jar
---
+++ nlp_sdks/semantic_simnet_bow_sdk/src/main/java/me/calvin/example/SemanticExample.java
@@ -0,0 +1,86 @@
+package me.calvin.example;
+
+import ai.djl.ModelException;
+import ai.djl.inference.Predictor;
+import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ZooModel;
+import ai.djl.translate.TranslateException;
+import me.calvin.aias.Lac;
+import me.calvin.aias.SimnetBow;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * 计算短文本相似度
+ *
+ * @author calvin
+ * @mail 179209347@qq.com
+ * @website www.aias.top
+ */
+
+public final class SemanticExample {
+
+  private static final Logger logger = LoggerFactory.getLogger(SemanticExample.class);
+
+  private SemanticExample() {}
+
+  public static void main(String[] args) throws IOException, TranslateException, ModelException {
+
+    // 分词
+    Lac lac = new Lac();
+    Criteria<String, String[][]> lacCriteria = lac.criteria();
+    // 计算短文本相似度
+    SimnetBow simnetBow = new SimnetBow();
+    Criteria<String[][], Float> simnetBowCriteria = simnetBow.criteria();
+
+    try (ZooModel<String, String[][]> lacModel = lacCriteria.loadModel();
+        Predictor<String, String[][]> lacPredictor = lacModel.newPredictor();
+        ZooModel<String[][], Float> simnetBowModel = simnetBowCriteria.loadModel();
+        Predictor<String[][], Float> simnetBowPredictor = simnetBowModel.newPredictor()) {
+
+      String input1 = "这个棋局太难了";
+      String input2 = "这个棋局不简单";
+      String input3 = "这个棋局很有意思";
+
+      logger.info("短文本 1: {}", input1);
+      String[][] lacResult1 = lacPredictor.predict(input1);
+      // 分词
+      logger.info("Words : " + Arrays.toString(lacResult1[0]));
+      logger.info("Tags : " + Arrays.toString(lacResult1[1]));
+      String[][] lacResult2 = lacPredictor.predict(input2);
+
+      logger.info("短文本 2: {}", input2);
+      // 分词
+      logger.info("Words : " + Arrays.toString(lacResult2[0]));
+      logger.info("Tags : " + Arrays.toString(lacResult2[1]));
+      String[][] lacResult3 = lacPredictor.predict(input3);
+
+      logger.info("短文本 3: {}", input3);
+      // 分词
+      logger.info("Words : " + Arrays.toString(lacResult3[0]));
+      logger.info("Tags : " + Arrays.toString(lacResult3[1]));
+
+      // 计算短文本相似度
+      int maxLength = Math.max(lacResult1[0].length, lacResult2[0].length);
+      String[][] inputs = new String[2][maxLength];
+      inputs[0] = lacResult1[0];
+      inputs[1] = lacResult2[0];
+      Float simResult1 = simnetBowPredictor.predict(inputs);
+      logger.info("短文本 1: {}", input1);
+      logger.info("短文本 2: {}", input2);
+      logger.info("相似度 : " + simResult1.floatValue());
+
+      maxLength = Math.max(lacResult1[0].length, lacResult3[0].length);
+      inputs = new String[2][maxLength];
+      inputs[0] = lacResult1[0];
+      inputs[1] = lacResult3[0];
+      Float simResult2 = simnetBowPredictor.predict(inputs);
+      logger.info("短文本 1: {}", input1);
+      logger.info("短文本 3: {}", input3);
+      logger.info("相似度 : " + simResult2.floatValue());
+    }
+  }
+}
