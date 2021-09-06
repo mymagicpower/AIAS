66f919caf5fea09b035611a71c1f9a5a36c7420a
Calvin <179209347@qq.com>
2021-09-06 20:56:35 +0800

    phoneme sdk

---
+++ voice_sdks/ph_sdk/src/main/java/me/calvin/example/SequenceExample.java
@@ -0,0 +1,20 @@
+package me.calvin.example;
+
+import me.calvin.aias.SequenceUtils;
+
+/**
+ * 文本转为拼音
+ * 拼音转为音素
+ * 文本转为音素
+ * 文本转为ID列表
+ */
+public class SequenceExample {
+    public static void main(String[] args) {
+        System.out.println(SequenceUtils.text2pinyin("文本转为拼音。"));
+        System.out.println(SequenceUtils.pinyin2phoneme(SequenceUtils.text2pinyin("拼音转为音素。")));
+        // 文本转为音素，用中文音素方案。
+        // 中文转为拼音，按照清华大学方案转为音素，分为辅音、元音、音调。
+        System.out.println(SequenceUtils.text2phoneme("文本转为音素。"));
+        System.out.println(SequenceUtils.text2sequence("文本转为ID列表。"));
+    }
+}
